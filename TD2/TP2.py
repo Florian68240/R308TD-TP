07aac38687772810fdc7ee291f1bc642be60dab6
Florian68240 <merckle.florian@orange.fr>
2023-09-21 16:18:38 +0200

    td

--- TD2/TP2.py
+++ TD2/TP2.py
@@ -6,38 +6,120 @@ class Personnage:
         self.initiative = niveau
 
     def attaque(self, autre_personnage):
-        if self.initiative > autre_personnage.initiative:
-            autre_personnage.points_de_vie -= self.niveau
-            if autre_personnage.points_de_vie > 0:
-                self.points_de_vie -= autre_personnage.niveau
-        elif self.initiative < autre_personnage.initiative:
-            self.points_de_vie -= autre_personnage.niveau
-            if self.points_de_vie > 0:
-                autre_personnage.points_de_vie -= self.niveau
-        else:
-            autre_personnage.points_de_vie -= self.niveau
-            self.points_de_vie -= autre_personnage.niveau
+        degats = self.degats()
+        autre_personnage.points_de_vie -= degats
+
+        print(f"{self.pseudo} attaque et inflige {degats} dégâts à {autre_personnage.pseudo}")
 
     def combat(self, autre_personnage):
         while self.points_de_vie > 0 and autre_personnage.points_de_vie > 0:
             self.attaque(autre_personnage)
-            if autre_personnage.points_de_vie <= 0:
-                print(f"{autre_personnage.pseudo} a été vaincu par {self.pseudo}!")
-            elif self.points_de_vie <= 0:
-                print(f"{self.pseudo} a été vaincu par {autre_personnage.pseudo}!")
+            print(f"{self.pseudo}: Points de vie restants = {self.points_de_vie}")
+            print(f"{autre_personnage.pseudo}: Points de vie restants = {autre_personnage.points_de_vie}")
 
     def soigner(self):
         self.points_de_vie = self.niveau
 
-# Test de la classe
+    def degats(self):
+        return self.niveau
+
+
+class Guerrier(Personnage):
+    def __init__(self, pseudo, niveau=1):
+        super().__init__(pseudo, niveau)
+        self.points_de_vie = niveau * 8 + 4
+        self.initiative = niveau * 4 + 6
+
+    def degats(self):
+        return self.niveau ** 2
+
+
+class Mage(Personnage):
+    def __init__(self, pseudo, niveau=1):
+        super().__init__(pseudo, niveau)
+        self.points_de_vie = niveau * 5 + 10
+        self.initiative = niveau * 6 + 4
+        self.mana = niveau * 5
+
+    def degats(self):
+        if self.mana >= 4:
+            self.mana -= 4
+            return self.niveau + 3
+        else:
+            return self.niveau
+
+    def attaque(self, autre_personnage):
+        degats_mage = self.degats()
+        degats_autre_personnage = autre_personnage.degats()
+
+        autre_personnage.points_de_vie -= degats_mage
+        self.points_de_vie -= degats_autre_personnage
+
+        print(f"{self.pseudo} attaque et inflige {degats_mage} dégâts à {autre_personnage.pseudo}")
+        print(f"{autre_personnage.pseudo} attaque et inflige {degats_autre_personnage} dégâts à {self.pseudo}")
+
+
+class Joueur:
+    def __init__(self, nom, nombre_max_personnages):
+        self.nom = nom
+        self.nombre_max_personnages = nombre_max_personnages
+        self.personnages = []
+
+    def ajouter_personnage(self, personnage):
+        if len(self.personnages) < self.nombre_max_personnages:
+            self.personnages.append(personnage)
+            print(f"{personnage.pseudo} a été ajouté à la liste des personnages de {self.nom}.")
+        else:
+            print(f"Impossible d'ajouter {personnage.pseudo}. Le nombre maximum de personnages est atteint.")
+
+    def get_personnage_par_numero(self, numero):
+        if 1 <= numero <= len(self.personnages):
+            return self.personnages[numero - 1]
+        else:
+            return None
+
+    def get_personnage_par_pseudo(self, pseudo):
+        for personnage in self.personnages:
+            if personnage.pseudo == pseudo:
+                return personnage
+        return None
+
+    def eliminer_personnage_par_numero(self, numero):
+        personnage = self.get_personnage_par_numero(numero)
+        if personnage:
+            self.personnages.remove(personnage)
+            print(f"{personnage.pseudo} a été éliminé de la liste des personnages de {self.nom}.")
+        else:
+            print(f"Aucun personnage trouvé avec le numéro {numero}.")
+
+    def eliminer_personnage_par_pseudo(self, pseudo):
+        personnage = self.get_personnage_par_pseudo(pseudo)
+        if personnage:
+            self.personnages.remove(personnage)
+            print(f"{personnage.pseudo} a été éliminé de la liste des personnages de {self.nom}.")
+        else:
+            print(f"Aucun personnage trouvé avec le pseudo {pseudo}.")
+
+# Exemple d'utilisation
 if __name__ == "__main__":
-    joueur1 = Personnage("Joueur1", 3)
-    joueur2 = Personnage("Joueur2", 2)
+    # Création de deux personnages
+    joueur1 = Joueur("Joueur 1", nombre_max_personnages=3)
 
-    print(f"{joueur1.pseudo} (Niveau {joueur1.niveau}) vs {joueur2.pseudo} (Niveau {joueur2.niveau})")
+    personnage1 = Personnage("Guerrier", niveau=3)
+    personnage2 = Guerrier("Guerrier 2", niveau=2)
+    personnage3 = Mage("Mage 1", niveau=3)
 
-    joueur1.combat(joueur2)
+    # Ajout des personnages au joueur
+    joueur1.ajouter_personnage(personnage1)
+    joueur1.ajouter_personnage(personnage2)
+    joueur1.ajouter_personnage(personnage3)
 
-    # Soigner le joueur1 après le combat
-    joueur1.soigner()
-    print(f"{joueur1.pseudo} a été soigné et a maintenant {joueur1.points_de_vie} points de vie.")
+    # Combat entre les personnages
+    joueur1.get_personnage_par_numero(1).combat(joueur1.get_personnage_par_numero(2))
+
+    # Soigner un personnage
+    joueur1.get_personnage_par_pseudo("Guerrier").soigner()
+    print(f"{joueur1.get_personnage_par_pseudo('Guerrier').pseudo} a été soigné.")
+
+    # Élimination d'un personnage
+    joueur1.eliminer_personnage_par_pseudo("Mage 1")
